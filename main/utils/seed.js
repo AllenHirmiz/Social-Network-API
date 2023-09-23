98713e9568c1b88e0613a88e11399c561e167617
AllenHirmiz <allenaliraqi@gmail.com>
2023-09-23 14:13:02 +1000

    add seeding file to the project

---
+++ main/utils/seed.js
@@ -0,0 +1,37 @@
+const { User, Thought, Reaction } = require("../models");
+const mongoose = require("mongoose");
+
+const connection = require("../config/connection");
+
+// Seed data
+const users = [
+  {
+    username: "Ryan",
+    email: "ryan@gmail.com",
+    thought: [],
+  },
+];
+
+const thoughts = [
+  {
+    username: "AllenH",
+    thoughtText: "this is a test",
+    reaction: [],
+  },
+];
+
+console.log(connection);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  await User.deleteMany({});
+
+  await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
+
+  console.table(users);
+  console.table(thoughts);
+  console.info("Seeding complete!");
+  process.exit(0);
+});
